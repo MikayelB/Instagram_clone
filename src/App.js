a4819a6a9ccd4f4833b7db34d47651f9cff584ba
Mikayel <mikael.bejanyan@gmail.com>
2022-08-27 17:38:56 +0400

    Header logo done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      {/* <h1>JK</h1> */}
       {/* Header */}
       <div className="app_header">
         <img
-          className="app_headerImage"
+          className="app_header_image"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png"
           alt="image not loading"
         />
